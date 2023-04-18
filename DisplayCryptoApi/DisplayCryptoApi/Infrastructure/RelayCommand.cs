f3a306a4c7b94c9f9365aa7cadd53421000349f5
VitaliyDenysiuk <denisuyk.vitaliy@gmail.com>
2023-04-18 14:33:29 +0300

    Create class RelayCommand

---
+++ DisplayCryptoApi/DisplayCryptoApi/Infrastructure/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DisplayCryptoApi.Infrastructure
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> action;
+        private readonly Predicate<object> predicate;
+
+        public RelayCommand(Action<object> action, Predicate<object> predicate)
+        {
+            this.action = action;
+            this.predicate = predicate;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return predicate == null || predicate(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            action(parameter);
+        }
+    }
+}
